958fbe29773d197d42a8a5053fe350ea7a3cd56f
Justin Purnell <jpurnell@users.noreply.github.com>
2022-03-21 19:24:08 -0400

    Updated required sample size

--- Sources/BusinessMath/Statistics/Probability Distribution/requiredSampleSize.swift
+++ Sources/BusinessMath/Statistics/Probability Distribution/requiredSampleSize.swift
@@ -11,3 +11,13 @@ import Numerics
 public func requiredSampleSize<T: Real>(z: T, stdDev: T, sampleMean: T, populationMean: T) -> T {
     return (T.pow(z, T(2)) * T.pow(stdDev, T(2)))/T.pow((sampleMean - populationMean), T(2))
 }
+
+public func requiredSampleSize<T: Real>(ci: T, stdDev: T, sampleMean: T, populationMean: T) -> T {
+    let z = zScore(ci: ci)
+    return requiredSampleSize(z: z, stdDev: stdDev, sampleMean: sampleMean, populationMean: populationMean)
+}
+
+public func requiredSampleSizeProb<T: Real>(ci: T, prob: T, maxError: T) -> T {
+    let z = zScore(ci: ci)
+    return (T.pow(z, T(2)) * prob * (T(1) - prob))/(T.pow(maxError, T(2)))
+}
