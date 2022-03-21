aa3973e626c3e0bef3289a1825c17563ca5d119b
Justin Purnell <jpurnell@users.noreply.github.com>
2022-03-21 16:17:22 -0400

    Add swift-numerics dependency

--- Package.swift
+++ Package.swift
@@ -13,14 +13,16 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-numerics", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BusinessMath",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Numerics", package: "swift-numerics")
+            ]),
         .testTarget(
             name: "BusinessMathTests",
             dependencies: ["BusinessMath"]),
