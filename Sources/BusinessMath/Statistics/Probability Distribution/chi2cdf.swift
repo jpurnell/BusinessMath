7e52e7637f008e14c57934c9c9d7b7147e284fbb
Justin Purnell <jpurnell@users.noreply.github.com>
2022-03-21 18:37:05 -0400

    Chi2CDF from WineTaster

---
+++ Sources/BusinessMath/Statistics/Probability Distribution/chi2cdf.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  
+//
+//  Created by Justin Purnell on 3/21/22.
+//
+
+import Foundation
+import Numerics
+
+func chi2pdf<T: Real>(x: T, dF: Int) -> T {
+    var returnValue: T = 0
+    guard x != 0 else { return T(0) }
+    let limit = max(("\(x * T(1000))" as NSString).integerValue, 1)
+    if limit == 1 { return T(0) }
+    for i in 1...limit {
+        let x: T = T(i)/1000
+        let dF = T(dF)
+
+        let topLeft = T.pow(x, ((dF - 2) / 2))
+        let topRight = 1 / T.exp(x / 2)
+        let bottomLeft = T.pow(2, (dF / 2))
+        let bottomRight = T.gamma(dF / 2)
+
+        let top  = topLeft * topRight
+        let bottom = bottomLeft * bottomRight
+
+        returnValue += top / bottom
+    }
+        return returnValue / 1000
+}
