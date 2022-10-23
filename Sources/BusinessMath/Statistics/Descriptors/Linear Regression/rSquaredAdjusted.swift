ce2353c951cf36e63b02a8f92ecb835d983f8f18
Justin Purnell <jpurnell@users.noreply.github.com>
2022-10-22 20:42:24 -0400

    Add Adjusted R-Squared

--- Sources/BusinessMath/Statistics/Descriptors/Linear Regression/rSquaredAdjusted.swift
+++ Sources/BusinessMath/Statistics/Descriptors/Linear Regression/rSquaredAdjusted.swift
@@ -7,3 +7,13 @@
 
 import Foundation
 import Numerics
+
+public func rSquaredAdjusted<T: Real>(_ x: [T], _ y: [T], _ population: Population = .population, _ descriptors: T = T(1)) -> T {
+    if (x.count == y.count) == false { return T(0) }
+    let observations = T(x.count)
+    let baseL = T(1) - rSquared(x, y, population)
+    let baseC = observations - T(1)
+    let baseR = observations - descriptors - T(1)
+    let base = (baseL * baseC) / baseR
+    return T(1) - base
+}
