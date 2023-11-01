2bd307159091571756655fc92c81225d64bf31b6
Justin Purnell <jpurnell@users.noreply.github.com>
2023-10-31 21:21:55 -0400

    updating u in Triangular

--- Sources/BusinessMath/Simulation/distributionTriangular.swift
+++ Sources/BusinessMath/Simulation/distributionTriangular.swift
@@ -20,7 +20,7 @@ public func triangularDistribution<T: Real>(low a: T, high b: T, base c: T) -> T
 	}
     let fc = (c - a) / (b - a)
 	print("fc: \(fc)")
-    let u = T(Int(drand48() * 1_000_000_000))
+    let u = T(Int(drand48() * 1_000_000_000)) / T(1_000_000_000)
 	print("u: \(u)")
     if u > 0 && u < fc {
         let s = u * (b - a) * (c - a)
