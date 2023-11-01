2cf11273dd938c209d85a1d2289aa90494bc86c1
Justin Purnell <jpurnell@users.noreply.github.com>
2023-10-31 21:25:18 -0400

    Remove Triangular Distribution Debugging

--- Sources/BusinessMath/Simulation/distributionTriangular.swift
+++ Sources/BusinessMath/Simulation/distributionTriangular.swift
@@ -19,16 +19,12 @@ public func triangularDistribution<T: Real>(low a: T, high b: T, base c: T) -> T
 		// Fallback on earlier versions
 	}
     let fc = (c - a) / (b - a)
-	print("fc: \(fc)")
     let u = T(Int(drand48() * 1_000_000_000)) / T(1_000_000_000)
-	print("u: \(u)")
     if u > 0 && u < fc {
         let s = u * (b - a) * (c - a)
-		print("u:\(u) < fc:\(fc)\t\(s)\n returning \(a) + sqrt\(s):\t\(a + sqrt(s))")
         return a + sqrt(s)
     } else {
         let s = (1 - u) * (b - a) * (b - c)
-		print("u:\(u) > fc:\(fc)\t\(s)\n returning \(b) - sqrt\(s):\t\(b - sqrt(s))")
         return b - sqrt(s)
     }
 }
