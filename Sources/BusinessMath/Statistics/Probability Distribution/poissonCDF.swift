1180c2f23626a492b49df13d3064fc683cc345ad
Justin Purnell <jpurnell@users.noreply.github.com>
2024-05-18 15:06:46 -0400

    Generalized PoissonCDF, fixed Floor Function

--- Sources/BusinessMath/Statistics/Probability Distribution/poissonCDF.swift
+++ Sources/BusinessMath/Statistics/Probability Distribution/poissonCDF.swift
@@ -8,11 +8,13 @@
 import Foundation
 import Numerics
 
-public func poissonCDF<T: Real>(_ x: Int, µ: T) -> T {
+public func poissonCDF<T: Real>(_ x: T, µ: T) -> T {
 	if x < 0 { return T(0) } else {
 //		var accumulator: [T] = []
 //		let first = T.exp(-1 * µ)
-		return T.exp(-1 * µ) * (0..<x).map({T.pow(µ, T($0)) / T($0.factorial())}).reduce(T(0), +)
+		let dx: Double = x as! Double
+		let floor:Int = Int(floor(dx))
+		return T.exp(-1 * µ) * (0...floor).map({T.pow(µ, T($0)) / T($0.factorial())}).reduce(T(0), +)
 //		for i in 0..<x {
 //			let numerator = T.pow(µ, T(i))
 //			let denominator = i.factorial()
