18cc45d2f95bbf77aac8bcc2975d248577f1dece
Justin Purnell <jpurnell@users.noreply.github.com>
2024-05-19 11:24:43 -0400

    Gamma Distribution off by one

--- Sources/BusinessMath/Simulation/distributionGamma.swift
+++ Sources/BusinessMath/Simulation/distributionGamma.swift
@@ -10,5 +10,5 @@ import Numerics
 
 // From https://personal.utdallas.edu/~pankaj/3341/SP07/NOTES/lecture_week_8.pdf
 public func distributionGamma<T: Real>(r: Int, λ: T) -> T {
-	return (0...r).map({_ in distributionExponential(λ: λ) }).reduce(T(0), +)
+	return (0..<r).map({_ in distributionExponential(λ: λ) }).reduce(T(0), +)
 }
