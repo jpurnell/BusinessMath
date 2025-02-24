5065ffbe0b1c14e24ac4d247356b62a15ce368d2
Justin Purnell <jpurnell@users.noreply.github.com>
2025-02-24 15:20:45 -0500

    Update Swift Tools and concurrency check

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -24,7 +24,8 @@ let package = Package(
                 .product(name: "Numerics", package: "swift-numerics")
             ],
 			swiftSettings: [
-				.enableExperimentalFeature("StrictConcurrency")			  ]
+				.enableUpcomingFeature("StrictConcurrency")
+			]
 		),
         .testTarget(
             name: "BusinessMathTests",
