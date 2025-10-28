fda76a7fd0c24aa4c71a82ebb7958392ba12bd6a
Justin Purnell <jpurnell@users.noreply.github.com>
2025-10-27 23:37:48 -0400

    Expose ignored errors

---
+++ Sources/BusinessMath/Errors/ArrayError.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Justin Purnell on 5/28/24.
+//
+
+import Foundation
+
+enum ArrayError: Error {
+	case mismatchedLengths
+	case emptyArray
+}
---
+++ Sources/BusinessMath/Errors/MathError.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Justin Purnell on 5/28/24.
+//
+
+import Foundation
+
+
+enum MathError: Error {
+	case divisionByZero
+}
