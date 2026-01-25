e47f10c92ad4a1869f518e0d3851e092ba5aa565
Justin Purnell <jpurnell@users.noreply.github.com>
2026-01-25 16:13:24 -0500

    Verified all tutorial examples and added Playgrounds for each.

--- Examples/DocumentationVerifier.playground/Contents.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-import BusinessMath
-import OSLog
-import PlaygroundSupport
-
-
---
+++ Sources/BusinessMath/Extensions/TestEntity.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  BusinessMath
+//
+//  Created by Justin Purnell on 1/13/26.
+//
+
+import Foundation
